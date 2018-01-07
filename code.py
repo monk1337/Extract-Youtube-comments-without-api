b5ae89489fd01853a31be3d1c7e5496569383a14
Ayodhyankit Paul <3x3paul@gmail.com>
2018-01-07 16:38:38 +0530

    Update code.py

--- code.py
+++ code.py
@@ -6,6 +6,9 @@ driver=webdriver.Chrome()
 
 driver.get('https://www.youtube.com/watch?v=iFPMz36std4')  #example video 
 
+time.sleep(2)  #if you have slow internet , increase the value of sleep
+
+
 driver.execute_script('window.scrollTo(1, 500);')
 #Here is a trick , Youtube only load comments when you scroll just down of video , if you scroll bottom or elsewhere, comments will not load , so first scroll to that down part and wait for loading comments after that scroll to bottom or whenever you want 
 #now wait let load the comments
